310ed8093b67e1e9d098263fa4b57b70d81a70ac
Olimxon <olimxon401@gmail.com>
2024-10-04 01:12:18 +0500

    fix

--- frontend/src/Components/Home/index.jsx
+++ frontend/src/Components/Home/index.jsx
@@ -1,24 +1,15 @@
 import "../../assets/styles/_setting.scss"
-<<<<<<< HEAD
 import "./style.scss"
 import Introduction from "./Introduction"
-=======
 import TopRating from "./TopRatedPRT"
-
->>>>>>> bf2b852010aea03249f8775eb7d5f87db4118c84
 
 function Home() {
     return (
         <div className="home">
-<<<<<<< HEAD
             <Introduction />
-=======
-            Home
-
             <TopRating />
->>>>>>> bf2b852010aea03249f8775eb7d5f87db4118c84
         </div>
     )
 }
 
-export default Home
+export default Home;

05cdfbfc7e8febd8554136d2327f6ff9f40ad4ba
Olimxon <olimxon401@gmail.com>
2024-10-04 02:20:13 +0500

    fix

--- frontend/src/Components/Home/index.jsx
+++ frontend/src/Components/Home/index.jsx
@@ -1,26 +1,19 @@
-import "../../assets/styles/_setting.scss"
-import "./style.scss"
-import Introduction from "./Introduction"
-import SetUp from "./SetUp"
-import TopRating from "./TopRatedPRT"
-<<<<<<< HEAD
-import RecentUsers from "./RecentUsers"
-import "./style.scss"
-=======
+import "../../assets/styles/_setting.scss";
+import "./style.scss";
+import Introduction from "./Introduction";
+import SetUp from "./SetUp";
+import TopRating from "./TopRatedPRT";
+import RecentUsers from "./RecentUsers"; // Убедитесь, что этот импорт остается
 
->>>>>>> f807b96c74cdbfb6fbdda44f5e58e2ccb93e9003
 function Home() {
     return (
         <div className="home">
             <Introduction />
             <TopRating />
-<<<<<<< HEAD
-            <RecentUsers />
-=======
             <SetUp />
->>>>>>> f807b96c74cdbfb6fbdda44f5e58e2ccb93e9003
+            <RecentUsers /> {/* Убедитесь, что RecentUsers также отображается */}
         </div>
-    )
+    );
 }
 
 export default Home;

fbc64932cc9ff07705ff2eeaf776795b7da080dc
Asadbek <ilhomjonovasadbek123.com>
2024-10-04 00:55:39 +0500

    outlet

--- frontend/src/Components/Home/index.jsx
+++ frontend/src/Components/Home/index.jsx
@@ -1,10 +1,13 @@
 import "../../assets/styles/_setting.scss"
+import TopRating from "./TopRatedPRT"
 
 
 function Home() {
     return (
         <div className="home">
             Home
+
+            <TopRating />
         </div>
     )
 }

23233ed8aea5384606bcddd78474fa9671d6172e
Olimxon <olimxon401@gmail.com>
2024-10-18 18:10:16 +0500

    fix

--- frontend/src/Components/Navigation/nav/index.jsx
+++ frontend/src/Components/Navigation/nav/index.jsx
@@ -10,27 +10,32 @@ function Nav() {
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
-        <nav>
+        <nav id="navchik" className={isMenuOpen ? 'open' : ''}>
             <div className="main">
                 <div className="first">
-                    <NavLink to="/" className="logo">
+                    <NavLink to="/" className="logo" onClick={closeMenu}>
                         <img src={IMG} alt="Logo" />
                     </NavLink>
                 </div>
                 <div className="second">
                     <div className="links">
-                        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
-                        <NavLink to="/portfolio" className={({ isActive }) => (isActive ? "active" : "")}>Portfolios</NavLink>
-                        <NavLink to="/community" className={({ isActive }) => (isActive ? "active" : "")}>Community</NavLink>
-                        <NavLink to="/aboutUs" className={({ isActive }) => (isActive ? "active" : "")}>About Us</NavLink>
-                        <NavLink to="/contact" className={({ isActive }) => (isActive ? "active" : "")}>Contact</NavLink>
+                        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")} onClick={closeMenu}>Home</NavLink>
+                        <NavLink to="/portfolio" className={({ isActive }) => (isActive ? "active" : "")} onClick={closeMenu}>Portfolios</NavLink>
+                        <NavLink to="/community" className={({ isActive }) => (isActive ? "active" : "")} onClick={closeMenu}>Community</NavLink>
+                        <NavLink to="/aboutUs" className={({ isActive }) => (isActive ? "active" : "")} onClick={closeMenu}>About Us</NavLink>
+                        <NavLink to="/contact" className={({ isActive }) => (isActive ? "active" : "")} onClick={closeMenu}>Contact</NavLink>
                         <div className="buttons">
                             <Link to="/login">
-                                <button className="signup">Sign in</button>
+                                <button className="signup" onClick={closeMenu}>Sign in</button>
                             </Link>
                             <Link to="/register">
-                                <button className="login">Sign up</button>
+                                <button className="login" onClick={closeMenu}>Sign up</button>
                             </Link>
                         </div>
                     </div>
